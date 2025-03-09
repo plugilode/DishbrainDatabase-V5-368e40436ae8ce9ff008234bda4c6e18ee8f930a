2823c04dafbd73377c0be9ce35390e9606200ef5
Plugilo Inc <patrick.blanks@plugilo.com>
2025-03-09 07:39:51 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,26 +2,30 @@
 const nextConfig = {
   experimental: {
     esmExternals: true,
-    optimizeCss: true,
+    optimizeCss: true, // Ensure CSS is optimized
   },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.json$/,
       type: 'json',
     });
-    // Add node polyfills
+
+    // Add node polyfills (for working with node dependencies)
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
       path: false,
     };
+
+    // Ignore specific warnings related to modules (like punycode)
     config.ignoreWarnings = [
       { module: /node_modules\/punycode/ }
     ];
+
     return config;
   },
   env: {
-    PORT: "3001"
+    PORT: "3001",
   },
   images: {
     domains: ['localhost', 'your-production-domain.com'],
